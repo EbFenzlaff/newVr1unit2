0ec367f1306e9f0f5f0eaf9a12f0addf764fa69e
EbFenzlaff <111587038+EbFenzlaff@users.noreply.github.com>
2022-10-11 10:10:14 -0600

    Challenge 2.3

---
+++ challenge 2.2/Assets/scripts/player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{ int lives = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(lives);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
